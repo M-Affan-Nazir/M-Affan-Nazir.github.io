6f07202d1ff532cb094d1c5da1c4c2f7e2da78af
M-Affan-Nazir <m.affann80@hotmail.com>
2025-06-02 23:22:13 -0600

    Minor Fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -339,7 +339,7 @@ const experiences = [
     company_link: "https://www.dataannotationtech.com/", // Replace with actual URL
     icon: dat, // Ensure this icon is imported or defined
     iconBg: "black", // Adjust color as needed
-    date: "April 2024 – September 2024",
+    date: "April 2024 – August 2024",
     points: [
       "Evaluated 500+ generative AI code outputs — dry-running Python, JavaScript, and C++ snippets and gave feedback that boosted model accuracy.",
       "Evaluated RAG models’ code completions — uploaded incomplete codebases, judged outputs against fine-grained criteria, benchmarked multiple models, and supplied corrected code for model learning."
